10d27491088f70b966cbe5af38ac9a594cf58dd6
Kostia <aks20050504@gmail.com>
2026-02-16 20:34:10 +0200

    refactor: clean up project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./app/index.css";
+import { App } from "./app/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(<App />);
+}
